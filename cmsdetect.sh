48631764a784dc5cd40e4350ce59b6f5e3e6cda9
Exagon899 <niclas.maas@t-online.de>
2026-06-20 03:26:57 +0200

    Update cmsdetect.sh

--- cmsdetect.sh
+++ cmsdetect.sh
@@ -35,7 +35,7 @@ set -uo pipefail
 
 GREEN="\033[0;32m"
 YELLOW="\033[0;33m"
-RED="\033[0;31m"
+RED="\033[1;31m"
 CYAN="\033[0;36m"
 BOLD="\033[1m"
 NC="\033[0m"
@@ -560,7 +560,7 @@ import json, sys
 
 GREEN = "\033[0;32m"
 YELLOW = "\033[0;33m"
-RED = "\033[0;31m"
+RED = "\033[1;31m"
 CYAN = "\033[0;36m"
 BOLD = "\033[1m"
 NC = "\033[0m"
